9d45fe1db3bd58b09a40545fdf8f78e9e281ba1d
konrad101296 <konradslimak@o2.pl>
2024-02-27 19:48:15 +0100

    Add project files.

---
+++ week 2 lesson 4 exercise nr.3/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Enter the lenght of the rectangle:");
+double lenght = double.Parse(Console.ReadLine());
+Console.WriteLine("Enter the width of the rectangle");
+double width = double.Parse(Console.ReadLine());
+
+double pow1 = Math.Pow(lenght, 2);
+double pow2 = Math.Pow(width, 2);
+double sqrt = pow1 + pow2;
+sqrt = Math.Sqrt(sqrt);
+Console.WriteLine($"The diagonal is: {sqrt}");
